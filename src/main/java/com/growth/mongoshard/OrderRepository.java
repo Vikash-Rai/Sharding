22f25ed54c784008ac1c3262c32e57f694e203bf
VIKASH RAI <44333349+Vikash-Rai@users.noreply.github.com>
2024-09-20 01:22:18 +0530

    Update OrderRepository.java

--- src/main/java/com/growth/mongoshard/OrderRepository.java
+++ src/main/java/com/growth/mongoshard/OrderRepository.java
@@ -3,6 +3,7 @@ package com.growth.mongoshard;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface OrderRepository extends MongoRepository<Order, String> {
-
+    // Find orders by customerId (shard key)
+    List<Order> findByCustomerId(String customerId);
 }
 
